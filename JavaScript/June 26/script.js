3223c8c5305e43ed9baf4e6ee0371d2d82e0c5fd
Premsingh02 <theaaravkumar02@gmail.com>
2023-06-26 22:40:52 +0530

    Javascript started

---
+++ JavaScript/June 26/script.js
@@ -0,0 +1,44 @@
+// alert("Hello everyone");
+// console.log("Hello world");
+
+// const a = 23;
+// const b = "hi";
+
+// const result = a + b;
+// console.log(result);
+
+// const a = "dafsfa";
+// const b = +(-1); // urenary operator
+
+// const result = (+a)+10;
+// console.log(result); // Nan
+
+// function
+// function add(a,b){
+//     return a + b;
+// }
+
+// const a = parseInt(prompt("no 1"));
+// const b = parseInt(prompt("no 2"));
+// const result = a + b;
+// console.log(result);
+
+// let a ;
+// console.log(a); // undefined;
+
+// let obj = {
+//     name: "rahul mohan",
+//     age: 23,
+//     isMarried: false,
+// }
+
+// console.log(obj.name);
+// console.log(obj.age);
+// console.log(obj.isMarried);
+
+const obj = {
+    name: "rahul",
+}
+
+obj.name = "potato";
+console.log(obj.name);
