c9eafb6b5d05ca6d16bd8d73a080534827e07c80
Premsingh02 <theaaravkumar02@gmail.com>
2023-06-27 22:38:07 +0530

    more new topics

---
+++ JavaScript/June 27/script.js
@@ -0,0 +1,27 @@
+// console.log(1+2*3); 
+
+// console.log(1&3);
+
+// console.log(1&&4);
+
+// function add(a,b) {
+//     return a+b;
+// }
+
+// const a = 5;
+// const b = +"t";
+
+// add(a,b);
+
+// function b() {
+//     console.log("Hi");
+//     console.log("Hello");
+// }
+
+// function a() {
+//     const value = 23;
+//     console.log("Yo,");
+//     b();
+// }
+
+// a();
