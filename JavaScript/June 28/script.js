22ccd21976a0071acb251cdfe69cea395ff25e74
Premsingh02 <theaaravkumar02@gmail.com>
2023-06-28 22:23:48 +0530

    learning new topics

---
+++ JavaScript/June 28/script.js
@@ -0,0 +1,17 @@
+// creation phase
+// execution phase
+
+fun();
+
+function fun() {
+    console.log("Hello");
+}
+
+let a = "test";
+let b = "test2";
+let c = "test3";
+
+// arrow functions
+const fun2 = ()=>{
+    console.log("this is another way of creating functions");
+}
