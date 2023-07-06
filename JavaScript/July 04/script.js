2fcbe6f5e9e0a9eb2ed039475b83647dc81cb508
Premsingh02 <theaaravkumar02@gmail.com>
2023-07-06 17:26:32 +0530

    array done

---
+++ JavaScript/July 04/script.js
@@ -0,0 +1,24 @@
+const arr1 = [1,2,3,4,5,3];
+const arr2 = [6,7,8];
+
+// console.log(arr1.concat(arr2));
+// console.log(arr1);  
+// console.log(arr2);  
+
+// functional programming
+// pure functions
+// const newArray = arr1.concat(arr2);
+
+// impure functions
+// arr1.reverse();
+
+// console.log(arr1);
+
+// console.log(arr1.toString());
+
+// console.log(arr1.join(","));
+
+// console.log(arr1.indexOf(3)); 
+
+// console.log(arr1.lastIndexOf(3));
+
