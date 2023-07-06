8f87186ab3d1becd9ed3ae5cd7baa429dac9f6e0
Premsingh02 <theaaravkumar02@gmail.com>
2023-07-06 11:24:44 +0530

    firstclassFucntion done

--- JavaScript/July 03/firstclassfnc.js
+++ JavaScript/July 03/firstclassfnc.js
@@ -1,14 +1,40 @@
-function abc (test) {
-    test();
-    return;
-}
+// function abc (test) {
+//     test();
+//     return;
+// }
 
-function randomFunction () {
-    console.log("this is a random function");
-}
+// function randomFunction () {
+//     console.log("this is a random function");
+// }
 
-abc(randomFunction);
+// abc(randomFunction);
 
 // abc(() => {
 //     console.log("Hi I was run by abc");
 // });
+
+// you can also return a function
+
+function robot(instruction) {
+    let someJob = null;
+    switch (instruction) {
+        case 1:
+            someJob = function () {
+                console.log("say hi");
+            }
+            break;
+        case 2:
+            someJob = function () {
+                console.log("say bye");
+            }
+            break;
+    }
+    return someJob;
+}
+
+function door() {
+    const fun = robot(1);
+    fun();
+}
+
+door();
