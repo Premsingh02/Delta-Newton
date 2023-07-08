a8e8a0f2cdac686a56f9e0a6a54e44c7163886be
Premsingh02 <theaaravkumar02@gmail.com>
2023-07-08 22:31:39 +0530

    some changes

--- JavaScript/July 06/script.js
+++ JavaScript/July 06/script.js
@@ -1,12 +1,13 @@
 // const button = document.querySelector("#changeButton");
 
-// // allways use arrow functions for callbakcs.
+// allways use arrow functions for callbakcs.
 // const id = button.addEventListener("click",()=>{
 //     const h1 = document.querySelector("h1");
 //     h1.innerText= "Now I can change";
 // })
 
 const addTaskButton = document.querySelector("#addTaskButton");
+
 
 addTaskButton.addEventListener("click", ()=>{
     const input = document.querySelector("#taskInput");
